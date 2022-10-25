791f29a3d5c3a1dee74e5bd6222d8b35c40d8bf8
Jadefaze <mabnoz@gmail.com>
2022-10-25 14:28:13 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mytext[] = "with proper grammer, but the outcome is a piece of art,";
+
+	printf("%s", mytext);
+	return (0);
+}
