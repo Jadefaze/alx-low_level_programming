b963474041b9d52f70ff551c8a586f7a4343b77c
Jadefaze <mabnoz@gmail.com>
2022-10-25 07:19:03 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main _Entry point
+ *
+ * Description: 'printing using puts'
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
+	/*The main function to print using puts*/
 	char mystring[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(mystring);
