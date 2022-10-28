1c9f62610dd403027446358f7a24c74f315df935
Jadefaze <mabnoz@gmail.com>
2022-10-28 15:45:51 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,19 +16,20 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 		s[i] -= 32;
 
-	for (j = 0; j < 13; j++)
-	{
-	if (s[i] == spe[j])
-	{																if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		for (j = 0; j < 13; j++)
 		{
-			s[i + 1] -= 32;
-		}
-	}
- 	}		
-	return (s);
+			if (s[i] == spe[j])
+			{								
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+ 		}		
 	
 	}
+	return (s);
 }
