2e44ed596716b026dd5597a79ace8ccf34092e5a
Jadefaze <mabnoz@gmail.com>
2022-12-19 12:58:32 -0800

    rev print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse printing with putchar and pointers
+ * @s: takes in a string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	/* get the las inde before null */
+	int index;
+	for (index = 0; s[index] != '\0'; ++index)
+		;
+	/*print the chars from last to first*/
+	for (--index; index >= 0; --index)
+		_putchar(s[index]);
+	_putchar('\n');
+}
