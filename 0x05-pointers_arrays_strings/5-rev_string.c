89dbb5ce7ea5348802d83ec811d28170a69648ed
Jadefaze <mabnoz@gmail.com>
2022-12-19 13:11:48 -0800

    new

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+/**
+ * rev_string - reverse a string just
+ * @s: parameter
+ *
+ * return: void
+ */
+void rev_string(char *s)
+{
+	int len, i;
+	char ch;
+
+	for (len = 0; s[len] != '\0'; ++len)
+		;
+	for (i = 0; i < len/2; ++i)
+	{
+		ch = s[i];
+		s[i] = s[len - 1 - i]; /*array strings start at 0*/
+		s[len - 1 - i] = ch;
+	}
+}
