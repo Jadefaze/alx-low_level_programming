c11fbe5ac6c78fe15bb40979f1c14b009090c8a9
Jadefaze <mabnoz@gmail.com>
2022-12-19 13:13:13 -0800

    new

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@ void rev_string(char *s)
 
 	for (len = 0; s[len] != '\0'; ++len)
 		;
-	for (i = 0; i < len/2; ++i)
+	for (i = 0; i < len / 2; ++i)
 	{
 		ch = s[i];
 		s[i] = s[len - 1 - i]; /*array strings start at 0*/
