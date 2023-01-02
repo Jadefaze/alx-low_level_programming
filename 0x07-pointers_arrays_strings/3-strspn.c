74a833597cb0c6a064d3e1277b25712fd735c05f
Jadefaze <mabnoz@gmail.com>
2023-01-02 13:38:57 -0800

    4th task

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+/**
+ * _strspn - a func to get len of
+ *		a prefix substring
+ *
+ * @s: pointer to a string input
+ * @accept: substring prefix to look for
+ *
+ * Return: the number of bytes in the initial segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, f;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		f = 1; /*flag status*/
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				f = 0; /*success*/
+				break;
+			}
+			j++;
+		}
+		if (f == 1)
+			break;
+		i++;
+	}
+	return (i);
+}
