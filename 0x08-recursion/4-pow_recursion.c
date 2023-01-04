8ce81c368bc1752a07396479a708c5a849a43067
Jadefaze <mabnoz@gmail.com>
2023-01-04 11:22:48 -0800

    pow recursive

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+/**
+ * _pow_recursion - to find power with recursive func
+ * @x: the base
+ * @y: the power
+ * Return: the power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
