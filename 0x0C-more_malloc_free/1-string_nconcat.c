6904faf292a6bf5d4bc72d9abb96e48245f61549
Jadefaze <mabnoz@gmail.com>
2023-10-08 08:22:49 -0700

    more malloc 2 revisted

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,26 +13,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *new_str;
 	unsigned int i, j, len_s1, len_s2;
 
+	i = j = len_s1 = len_s2 = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s1 = "";
-	while (s1[len_s1])
+	while (s1[len_s1] != '\0')
 		len_s1++;
-	while (s2[len_s2])
+	while (s2[len_s2] != '\0')
 		len_s2++;
 	new_str = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 
 	if (new_str == NULL)
 		return (NULL);
-	for (i = 0; i < len_s1; i++)
+	while (s1[i] != '\0')
+	{
 		new_str[i] = s1[i];
-	if (n >= len_s2 - 1)
+		i++;
+	}
+	if (n >= len_s2)
 		n = len_s2;
 	while (j < n)
+	{
 		new_str[i] = s2[j];
 		i++;
 		j++;
+	}
 	new_str[i] = '\0';
 	return (new_str);
 }
