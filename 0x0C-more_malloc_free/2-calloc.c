cee60747902dec708a37d330f8e51ca3a3dec5ca
Jadefaze <mabnoz@gmail.com>
2023-10-08 14:57:02 -0700

    filled mem block with zero

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,13 +10,23 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *array_alloc;
+	unsigned int i, total_size;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	array_alloc = malloc(size * nmemb);
+	total_size = size * nmemb;
+
+	array_alloc = malloc(total_size);
 
 	if (array_alloc == NULL)
 		return (NULL);
+
+	while (i < total_size)
+	{
+		*((int *)array_alloc + i) = 0;
+		i++;
+	}
+
 	return (array_alloc);
 }
