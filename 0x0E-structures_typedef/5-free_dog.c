26a1c502b07fc1470351df1fb389a7934889a942
Jadefaze <mabnoz@gmail.com>
2023-10-12 02:34:21 +0300

    free mem struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - to free dog struct from memory
+ * @d: pointer to the struct
+ * Return: nothing (void)
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
