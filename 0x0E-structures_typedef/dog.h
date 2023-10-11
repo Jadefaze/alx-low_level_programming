7f4bdad46bc4471344f7aa4df8248d144fc559ac
Jadefaze <mabnoz@gmail.com>
2023-10-11 23:08:37 +0300

    new type - dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - to define a new type dog
+ * @name: dog name
+ * @age: dog name
+ * @owner: name owner of dog
+ * Description: Just a dog type
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif /*DOG*/
