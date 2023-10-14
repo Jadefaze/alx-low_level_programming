f5901326ad402747338543552b0085cabf954dc7
Jadefaze <mabnoz@gmail.com>
2023-10-14 03:00:57 +0300

    print string params

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,14 +19,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		return;
 
 	va_start(args, n);
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, int);
-		if (separator != NULL)
-			printf("%i%s", num, separator);
-		else
-			printf("%i", num);
+		printf("%i", num);
+
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
-	num = va_arg(args, int);
-	printf("%i\n", num);
+	va_end(args);
+	printf("\n");
 }
