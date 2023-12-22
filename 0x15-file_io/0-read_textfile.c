c48f504b774f34e6ccd427b335b69b448809c738
Jadefaze <mabnoz@gmail.com>
2023-12-23 01:19:24 +0300

    task 0 redone

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,31 +11,41 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	int fp;
-	ssize_t bytesRead;
-	ssize_t dataWrite;
+	int fd;
+	ssize_t bytesRead, bytesWritten;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	fp = open(filename, O_RDONLY);
+	buffer = (char *)malloc(sizeof(size_t) * letters);
 
-	if (!fp)
+	if (buffer == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	bytesRead = read(fp, buf, letters);
-	if (!bytesRead)
-		return (0);
-	dataWrite = write(STDOUT_FILENO, buf, bytesRead);
 
-	if (!dataWrite)
-		return (0);
-	free(buf);
-	close(fp);
+	bytesRead = read(fd, buffer, letters);
 
-	return (dataWrite);
+	if (bytesRead == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+
+	if (bytesWritten == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+
+	return (bytesWritten);
 }
