26be1f1b2e1adb012e471b4ec23adc15a5b688ec
Jadefaze <mabnoz@gmail.com>
2023-12-25 17:05:31 +0300

    static ilibraries: task 1

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - to locate a substring
+ * @haystack: the string to search
+ * @needle: the substring to locate
+ * Return: pointer to start of located substring, or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	(void)haystack;
+	(void)needle;
+
+	return (NULL);
+}
