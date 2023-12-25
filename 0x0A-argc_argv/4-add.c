72c6fbd5801329b3d791dcb84669d113e6c82bd9
Jadefaze <mabnoz@gmail.com>
2023-12-25 22:28:32 +0300

    task 5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main -  to add args passed
+ * @argc: the number of args passed(command line args)
+ * @argv: argument vector
+ * Return: 0 for success, else -1
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, i;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!(atoi(argv[i])))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
