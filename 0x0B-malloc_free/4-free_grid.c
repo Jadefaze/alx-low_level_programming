42bc2c8cb250c0e7bfe8afe344ed6d7b37fffcae
Jadefaze <mabnoz@gmail.com>
2023-12-26 22:11:08 +0300

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * free_grid - to free previously created 2d array
+ * @grid: the 2d array
+ * @height: the number of rows to free
+ * Return: nothing (void)
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	i = height;
+
+	while (i >= 0)
+	{
+		free(grid[i]);
+		i--;
+	}
+	free(grid);
+}
