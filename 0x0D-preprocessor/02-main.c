eca693c7a1db1747b2555db2c2ceccf2c9e2e125
Jadefaze <mabnoz@gmail.com>
2024-01-04 17:18:01 +0300

    using inbuilt macros

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - to print the name of the program
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
