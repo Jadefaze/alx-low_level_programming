51ecac9a8279d342b4267243b44fa8197d402acf
Jadefaze <mabnoz@gmail.com>
2024-01-06 18:41:55 +0300

    wrapper function 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - to print a name (a person for example)
+ * @name: the name (of the person)
+ * @f: pointer to function
+ * Return: Nothing (void)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
