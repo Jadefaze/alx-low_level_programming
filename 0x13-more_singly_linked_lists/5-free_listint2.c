079856db4ad66860ec2b57dc1754c9301b0ede22
Jadefaze <mabnoz@gmail.com>
2024-01-26 01:08:44 +0300

    lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - to free memory allocated to list
+ * @head: the pointer to begining of list
+ * Return: nothing (void)
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current_ptr;
+	listint_t *next;
+
+	if (*head == NULL || head == NULL)
+		return;
+
+	current_ptr = *head;
+
+	while (current_ptr != NULL)
+	{
+		next = current_ptr->next;
+		free(current_ptr);
+		current_ptr = next;
+	}
+
+	*head = NULL;
+}
