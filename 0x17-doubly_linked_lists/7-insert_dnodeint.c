7fe27784176e2ec86ebffd19282b9fb4cf9409dc
Jadefaze <mabnoz@gmail.com>
2024-01-27 01:46:48 +0300

    dlists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,19 +10,28 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *current_ptr;
+	dlistint_t *new_node, *current_ptr;
 	unsigned int i;
 
-	/*allocate mem for new node*/
+	if (h == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
-
-	/* check if new node is not null */
 	if (new_node == NULL)
 		return (NULL);
+	/* initialise new node */
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 	current_ptr = *h;
-
 	i = 0;
 	/*iterate the list with current_ptr until index*/
 	while (i < idx - 1 && current_ptr != NULL)
@@ -35,16 +44,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(new_node);
 		return (NULL);
 	}
-
-	/*insert new node here */
-	new_node->n = n;
 	new_node->prev = current_ptr;
 	new_node->next = current_ptr->next;
-
 	if (current_ptr->next != NULL)
 		current_ptr->next->prev = new_node;
-
 	current_ptr->next = new_node;
-
 	return (new_node);
 }
